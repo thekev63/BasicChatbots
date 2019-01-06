d73d8562694316a773bda4c1eecc417c33d00fd4
Kevin McCarty <thekev63@users.noreply.github.com>
2019-01-05 19:49:28 -0800

    Initial Input

---
+++ Session 4 - Basic Input/BasicInput - Final/BasicInput/BasicInput/Dialogs/RootDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace HelloWorld.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        string userName = string.Empty;
+
+        // this is our entry point
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> activity)
+        {
+            var response = await activity;
+            PromptDialog.Text(
+                context: context,
+                resume: ResumeGetName,
+                prompt: "How would you like to be called?"
+            );
+        }
+        public virtual async Task ResumeGetName(IDialogContext context, IAwaitable<string> userResponse)
+        {
+            string response = await userResponse;
+            userName = response;
+            await context.PostAsync($"Welcome to this demo, {userName}!");
+            context.Done(this);
+        }
+    }
+}
---
+++ Session 4 - Basic Input/BasicInput - Step 1/BasicInput/BasicInput/Dialogs/RootDialog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace HelloWorld.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        // this is our entry point
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> activity)
+        {
+            var response = await activity;
+        }
+    }
+}
