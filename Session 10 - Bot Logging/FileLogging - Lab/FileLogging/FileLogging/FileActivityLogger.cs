a96acc89d69fb51694a445764b203bc050c8fd43
Kevin McCarty <thekev63@users.noreply.github.com>
2019-01-06 17:54:01 -0800

    Initial Upload

---
+++ Session 10 - Bot Logging/FileLogging - Lab/FileLogging/FileLogging/FileActivityLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Threading.Tasks;
+// for logging
+using Microsoft.Bot.Builder.History;
+using Microsoft.Bot.Connector;
+using System.IO;
+
+// disable warnings for now
+#pragma warning disable 1998
+
+namespace FileLogging
+{
+    public class FileActivityLogger : IActivityLogger
+    {
+        TextWriter tw;
+
+        // TODO - create a constructor taking a TextWriter as input, assign to tw
+
+        public async Task LogAsync(IActivity activity)
+        {
+			// TODO - complete this method
+			// Log activity to output and to a file
+			// Apply a filter for extra credit
+        }
+    }
+}
---
+++ Session 10 - Bot Logging/FileLogging - Lab/FileLogging/FileLogging/Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Autofac;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Azure;
+using System.Configuration;
+using System.Reflection;
+using System.IO;
+
+namespace FileLogging
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+		// TODO - add a TextWriter
+
+        protected void Application_Start()
+        {
+			// TODO - instantiate a writer
+
+            // register various services we will need for the operation of our bot
+            Conversation.UpdateContainer(
+              builder =>
+              {
+                  // these first two registrations allow us to manage bot state
+                  builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+                  var store = new InMemoryDataStore();
+
+
+                  builder.Register(c => store)
+                            .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                            .AsSelf()
+                            .SingleInstance();
+
+                  // TODO - register our logger here
+
+              });
+
+                     
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
