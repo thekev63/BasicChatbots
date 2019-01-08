c848bd546362ce3ffcf3e8cd8657a08ef3e98e84
Kevin McCarty <thekev63@users.noreply.github.com>
2019-01-07 20:50:13 -0800

    Initial Upload

---
+++ Session 11 - QnA Bot/QNA - Final/QNA/QNA/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Autofac;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Azure;
+using System.Configuration;
+using System.Reflection;
+
+namespace QNA
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Conversation.UpdateContainer(
+              builder =>
+              {
+                  builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+                  var store = new InMemoryDataStore();
+
+
+                  builder.Register(c => store)
+                            .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                            .AsSelf()
+                            .SingleInstance();
+              });
+
+                     
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
---
+++ Session 11 - QnA Bot/QNA - Step 1/QNA/QNA/Dialogs/RootDialog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using QnAMakerDialog;
+
+namespace QNA.Dialogs
+{
+    [Serializable]
+    [QnAMakerService("https://kmqnademo.azurewebsites.net/qnamaker", "0dea50c6-4225-4f01-bf05-dfec836663f7", 
+        "20dc628e-56d2-4176-a7e8-681511bbdae9")]
+    public class RootDialog : QnAMakerDialog<object>
+    {
+
+    }
+}
